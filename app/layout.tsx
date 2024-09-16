90ca71f209e19887b0ceea1d3a47adceec3bb21b
Jeremiah <jayonojah@gmail.com>
2024-09-16 19:36:53 +0100

    Landing Page Clean-up

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        <Toaster />
+        <Toaster richColors />
         {children}
       </body>
     </html>
