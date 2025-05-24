ddcd5e5f94c32a59b8aad167e2a9f8a55782ecbd
Aadji Mukhtar <alfadaji@gmail.com>
2025-05-24 18:20:18 +0100

    added the contact us page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,10 +1,14 @@
 import Banner from "@/components/contact-us/banner";
+import Footer from "@/components/footer";
+import NewsletterSubscription from "@/components/home/news-letter";
 import React from "react";
 
 const Page = () => {
   return (
     <div>
       <Banner />
+      <NewsletterSubscription />
+      <Footer />
     </div>
   );
 };
--- components/contact-us/banner.tsx
+++ components/contact-us/banner.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Navbar from "../navbar";
+import ContactForm from "./form";
 
 const Banner = () => {
   return (
     <div>
       <Navbar theme="light" />
+      <div className="app-width py-32 flex flex-col md:flex-row justify-between gap-10">
+        <div className="space-y-4">
+          <h1 className="text-4xl lg:text-5xl font-bold text-gray-900">
+            Get in touch
+          </h1>
+          <p className="text-gray-600 text-lg max-w-[316px]">
+            Let us know how we can support you or answer any question
+          </p>
+        </div>
+        <ContactForm />
+      </div>
     </div>
   );
 };
